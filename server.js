131bbf5815d247258dd5b0231505dbf23452395d
nitsan <nitsanf4@gmail.com>
2019-01-02 13:50:05 +0200

    Fix Routing

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev', { stream: logger.stream }));
 
-app.use('/midis', midis);
+app.use(midis);
 
 // Index route
 app.get('/', (req, res) => {
