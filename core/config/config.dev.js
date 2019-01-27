382566736d060f8fc2954062f06735590a457c6f
nitsan <nitsanf4@gmail.com>
2019-01-27 17:43:58 +0200

    Adding Spotify user saving

--- core/config/config.dev.js
+++ core/config/config.dev.js
@@ -9,9 +9,9 @@ config.dbPort = process.env.dbPort || '27017';
 config.remoteDbHost = process.env.remoteDbHost || 'ds145484.mlab.com';
 config.remoteDbPort = process.env.remoteDbPort || '45484';
 config.dbName = process.env.dbName || 'midis';
-config.dbStat = process.env.dbStat || 'local'; // "remote"
+config.dbStat = process.env.dbStat || 'remote'; // "local"
 config.dbUserName = process.env.dbUserName || 'smartinstruments';
-config.dbPassword = process.env.dbPassword; // || 'smartinstruments1dc';
+config.dbPassword = process.env.dbPassword;
 config.serverPort = process.env.PORT || 5000;
 
 export default config;
